868cb86ab70fea5ad40bc1162314b2442081c593
User001 <rp527@mail.ru>
2023-07-23 15:48:21 +0300

    Задание№3

--- main.py
+++ main.py
@@ -51,3 +51,35 @@ def get_shop_list_by_dishes(dishes, person_count):
     pprint(result, sort_dicts=False)
 
 get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Фахитос'], 2)
+
+
+#ЗАДАНИЕ 3
+
+import os
+
+current = os.getcwd()
+folder = 'sorted'
+file_name_1 = '1.txt'
+file_name_2 = '2.txt'
+file_name_3 = '3.txt'
+file_name_4 = 'new.txt'
+
+with open(os.path.join(current, folder, file_name_1), encoding='utf-8') as file1, open(os.path.join(current, folder, file_name_2), encoding='utf-8') as file2, open(os.path.join(current, folder, file_name_3), encoding='utf-8') as file3, open(os.path.join(current, folder, file_name_4), 'w') as new_file:
+    dict_file = {
+        file1 : [len(file1.readlines()), file_name_1],
+        file2 : [len(file2.readlines()), file_name_2],
+        file3 : [len(file3.readlines()), file_name_3]
+    }
+
+    dict_file_sort = dict(sorted(dict_file.items(), key=lambda item : item[1][0]))
+    # pprint(dict_file_sort, sort_dicts=False)
+    file1.seek(0)
+    file2.seek(0)
+    file3.seek(0)
+
+    for keys, values in dict_file_sort.items():
+        new_file.writelines(f'{values[1]}\n')
+        new_file.writelines(f'{values[0]}\n')
+        for line in keys.readlines():
+            new_file.writelines(f'{line}')
+        new_file.writelines('\n')
